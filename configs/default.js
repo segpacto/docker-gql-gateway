ef6ad20e0cad674554dec12b33534a539732a030
Ivan <segpacto@yahoo.es>
2021-12-11 17:06:17 +0100

    feat: Configuring Dockerfile & implementing load of configurations & setting default values

---
+++ configs/default.js
@@ -0,0 +1,11 @@
+const logger = console
+
+module.exports = {
+  port: 5000,
+  localSchema: '',
+  resolvers: {},
+  endpointsList: [],
+  apolloServerConfig: {},
+  contextConfig: {},
+  logger
+}
